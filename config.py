96d45f72f0d02f2f13a08e86265b38984c306a68
Aldryn Gutierrez <aldryn.gutierrez@gmail.com>
2017-12-04 11:46:05 +0800

    Update Config and Setup Py Files

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-relationaldb_host='carpal-api.cluwsec6wqyi.ap-southeast-1.rds.amazonaws.com'
-relationaldb_username='cpadm'
-relationaldb_password='2U#u[}aB5sVX6<3_'
-relationaldb_dbname='testingdb'
+relationaldb_host=''
+relationaldb_username=''
+relationaldb_password=''
+relationaldb_dbname=''

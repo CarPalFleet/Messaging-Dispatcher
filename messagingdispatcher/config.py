9f582f0345b6f75ed4bfde545bd8d7186f8f5392
Nick Chen <ccwukong@gmail.com>
2017-12-13 14:22:20 +0800

    minor fix

--- messagingdispatcher/config.py
+++ messagingdispatcher/config.py
@@ -1,6 +1,6 @@
 CONFIG = {
-   'relationaldb_host': 'carpal-api.cluwsec6wqyi.ap-southeast-1.rds.amazonaws.com',
-   'relationaldb_username':'cpadm',
-   'relationaldb_password':'2U#u[}aB5sVX6<3_',
-   'relationaldb_dbname':'testingdb',
-   'elasticsearch_endpoint':'https://search-carpal-dev-test-3vrbvy2dlqbg62umur336ictjy.ap-southeast-1.es.amazonaws.com'}
+   'relationaldb_host': '',
+   'relationaldb_username':'',
+   'relationaldb_password':'',
+   'relationaldb_dbname':'',
+   'elasticsearch_endpoint':''}

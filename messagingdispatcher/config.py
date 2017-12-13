85f21ef966b1ec7843f7dda52227b3f3881d2d9b
Nick Chen <ccwukong@gmail.com>
2017-12-13 13:29:37 +0800

    Updated upsert function to handle duplicated id issue for ES, revised test cases for upsert document in ES

--- messagingdispatcher/config.py
+++ messagingdispatcher/config.py
@@ -1,5 +1,5 @@
 CONFIG = {
-    'relationaldb_host': 'carpal-api.cluwsec6wqyi.ap-southeast-1.rds.amazonaws.com',
-    'relationaldb_username':'cpadm',
-    'relationaldb_password':'2U#u[}aB5sVX6<3_',
-    'relationaldb_dbname':'carpal',}
+   'relationaldb_host': 'carpal-api.cluwsec6wqyi.ap-southeast-1.rds.amazonaws.com',
+   'relationaldb_username':'cpadm',
+   'relationaldb_password':'2U#u[}aB5sVX6<3_',
+   'relationaldb_dbname':'testingdb'}
--- tests/test_elasticsearch.py
+++ tests/test_elasticsearch.py
@@ -1,6 +1,6 @@
 import os, sys, unittest
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/../')
-from messagingdispatcher.search.esclient import ElasticSearchDocumentModel
+from messagingdispatcher.search.esclient import ElasticSearchDocumentModel, ElasticSearch
 
 class TestElasticSearch(unittest.TestCase):
     def setUp(self):
@@ -14,10 +14,16 @@ class TestElasticSearch(unittest.TestCase):
                                                         "order_status_name": "Pending",
                                                         "pickup_date_time": "2015-11-28 16:30",
                                                         "driver_id": 0,
-                                                        "driver_name": ""
+                                                        "driver_name": "Nick Chen"
                                                     })
 
     def test_document_model(self):
-         self.assertEqual(self._new_doc.index, 'order')
-         self.assertEqual(self._new_doc.doc_type, 'default')
-         self.assertEqual(self._new_doc.doc_id, '1')
+        self.assertEqual(self._new_doc.index, 'order')
+        self.assertEqual(self._new_doc.doc_type, 'default')
+        self.assertEqual(self._new_doc.doc_id, '1')
+
+    def test_upsert_document(self):
+        es = ElasticSearch(host='https://search-carpal-dev-test-3vrbvy2dlqbg62umur336ictjy.ap-southeast-1.es.amazonaws.com')
+        result = es.upsert_document(self._new_doc)
+        print(result)
+        self.assertEqual(result.get('_shards').get('successful'), 1)
